3e263de25e23228f5a1d3b0d453512876795bf66
ashwinreddy1894 <ashwinreddy1894@gmail.com>
2024-07-16 11:13:50 +0530

    Update oops.java

--- oops.java
+++ oops.java
@@ -1,5 +1,6 @@
 public class oops{
     public static void main(String[] args) {
         System.out.println("today i will use github to create an oops project");
+        System.out.println("another small change");
     }
 }
